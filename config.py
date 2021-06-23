5691b732c80b8f1cc2680efd21929253fe1e076e
Kanin <im@kanin.dev>
2021-06-22 23:55:59 -0400

    Uhhh testing Docker but I have no clue what I'm doing.

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+
+class Config:
+    # Running
+    DEBUG = os.getenv("DEBUG", False)
+
+    # Theme
+    DARK_THEME = os.getenv("DARK_THEME", False)
+
+    # Auth
+    AUTH_PASSWORD = os.getenv("PASSWORD", "supersecret")
+    SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(64).hex())
+
+    # Pathing
+    NGINX_PATH = "/etc/nginx"
+    SITES_AVAILABLE_PATH = os.path.join(NGINX_PATH, "sites-available")
+    SITES_ENABLED_PATH = os.path.join(NGINX_PATH, "sites-enabled")
